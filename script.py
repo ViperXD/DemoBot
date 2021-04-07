673c2402665f5341da9e34961479c5c455f70c39
TG Expert <78008481+v-v-r-official@users.noreply.github.com>
2021-04-07 12:31:38 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,11 @@
+START_TEXT="""<b>Hi,{}
 
+This Is A Demo Bot.Don't Waste Time!</b>"""
+
+HELP_TEXT="""<b>Hi,{}
+
+This Is A Demo Bot.Don't Waste Time!</b>"""
+
+ABOUT_TEXT="""<b>Hi,{}
+
+This Is A Demo Bot.Don't Waste Time!</b>"""
